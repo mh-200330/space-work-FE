6fa6a068e1e0f731a45e2de359ef1193bb73947b
Mariuca H <mariuca.hlihor@yaho.com>
2023-10-03 18:25:31 +0300

    ok

---
+++ 3octombrie/work1.js
@@ -0,0 +1,13 @@
+// console.log(2 + 3);
+
+//date de intrare
+// 10 4 2 5 8 
+
+//inceput
+
+//1. Citim cate un element din lista
+//2. Comparam primul element cu al doilea 
+//2.1 Daca primul este mai mic, atunci vom presupune ca acela este,
+
+//sfarsit
+
